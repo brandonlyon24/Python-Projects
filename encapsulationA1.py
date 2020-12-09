438ac07e4b0b30d2a4fbaece871792ff48833f6d
brandonlyon24 <72480983+brandonlyon24@users.noreply.github.com>
2020-12-09 14:08:30 -0800

    Create encapsulationA1.py

---
+++ encapsulationA1.py
@@ -0,0 +1,24 @@
+#
+#   Encapsulation Assignment
+#1. This class should make use of a private attribute or function.
+#2. This class should make use of a protected attribute or function.
+#3. Create an object that makes use of protected and private.
+#4. Add comments throughout your Python explaining your code.
+# Author:   Brandon Lyon
+#
+
+
+class Protected:
+    def __init__(self):
+        self.__privateVar = 10
+
+    def getPrivate(self):
+        print(self.__privateVar)
+
+    def setPrivate(self, private):
+        self.__privateVar = private
+
+obj = Protected()   # shortens the name 
+obj.getPrivate()    # gets the private number 
+obj.setPrivate(23)  # Sets the value to a new value 
+obj.getPrivate()    # Gets that new value 
